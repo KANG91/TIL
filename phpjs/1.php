2ffafc4ce6378f47461e19ef04fb79a1d92376a6
KANG91 <ledestin91@gmail.com>
2018-02-08 15:33:13 +0900

    basic of html, css, javascript

---
+++ phpjs/1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+
+    </title>
+  </head>
+  <body>
+    <h1>php</h1>
+    <?php
+      echo 10 + 10; //php 엔진이 이 부분을 해석해서 보여줌
+      ?>
+       <h1>JavaScript</h1>
+           <script>
+              document.write(10 + 10);
+
+           </script>
+  </body>
+</html>
---
+++ phpjs/9.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <h1>javascript</h1>
+    <ul>
+
+
+    <script type="text/javascript">
+      i = 0;
+
+      while(i < 10){
+        document.write("hello world<br />");
+        i = i + 1;
+      }
+      </ul>
+
+    </script>
+    <h2>php</h2>
+    <ul>
+
+    <?php
+      $i = 0 ;
+      while($i < 10){
+        <li> echo "hello world<br/>"</li>;
+          $i = $i + 1 ;
+      }
+      ?>
+      </ul>
+  </body>
+</html>
